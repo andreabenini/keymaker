fe2159b6ad4eb7f06a6740d52151f80251577ef7
Ben <andreabenini@users.noreply.github.com>
2026-04-23 15:54:25 +0200

    cli builder

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# The Keymaker - ESP32 CYD OTP Authenticator
+# Copyright (C) 2026 Andrea Benini
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# COMMERCIAL USAGE: If you wish to use this software in a commercial 
+# product or environment where GPLv3 compliance is not possible, 
+# please contact the creator of this repository [andreabenini] @ gmail
+# for a commercial license.
+#
+
+# Simple builder based on current ESP-IDF version
+. $HOME/.espressif/v5.5.3/esp-idf/export.sh && idf.py build
