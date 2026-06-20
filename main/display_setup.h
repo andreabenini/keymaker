f0b7478537b6b346d2c0c210d63c17e50a31abb5
Ben <andreabenini@users.noreply.github.com>
2026-06-20 17:31:44 +0200

    Header file, setup screen

---
+++ main/display_setup.h
@@ -0,0 +1,29 @@
+/*
+ * The Keymaker - ESP32 CYD OTP Authenticator
+ * Copyright (C) 2026 Andrea Benini
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * COMMERCIAL USAGE: If you wish to use this software in a commercial 
+ * product or environment where GPLv3 compliance is not possible, 
+ * please contact the creator of this repository [andreabenini] @ gmail
+ * for a commercial license.
+ */
+#ifndef DISPLAY_SETUP_H
+#define DISPLAY_SETUP_H
+
+#include "lvgl.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_lcd_touch.h"
+#include <stdbool.h>
+
+
+#endif // DISPLAY_SETUP_H
