22afbd2a4b4f96f8bdb8f36d5677c8f3921c25a2
Ben <andreabenini@users.noreply.github.com>
2026-07-12 09:46:40 +0200

    Setup page functions

---
+++ main/display_setup.c
@@ -0,0 +1,27 @@
+/*
+ * The Keymaker - ESP32 CYD OTP Authenticator
+ * Copyright (C) 2026 Andrea Benini
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * COMMERCIAL USAGE: If you wish to use this software in a commercial 
+ * product or environment where GPLv3 compliance is not possible, 
+ * please contact the creator of this repository [andreabenini] @ gmail
+ * for a commercial license.
+ */
+
+#include "display_setup.h"
+#include "display_main.h"
+#include "portal.h"
+#include "esp_log.h"
+
+static const char *TAG = "display_setup";
+
